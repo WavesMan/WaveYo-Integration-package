a7cc6c371d5e8b705beb9ffdc0472f459e2ab4f3
Waves_Man <104572398+WavesMan@users.noreply.github.com>
2024-04-10 21:07:32 +0800

    Update 1.删除原文件.bat
    删除了旧版本的代码多与内容

--- 1.删除原文件.bat
+++ 1.删除原文件.bat
@@ -7,32 +7,12 @@ chcp 65001 > nul 2>&1
 
 REM 设置文件列表，可以包含多个文件路径和文件夹路径，每个路径用空格分隔
 set "file_list=%file_list% BepInEx\."
-set "file_list=%file_list% BepInEx\cache\."
 
-set "file_list=%file_list% BepInEx\config\."
-set "file_list=%file_list% BepInEx\config\BrutalCompanyMinus\."
-set "file_list=%file_list% BepInEx\config\EliteMasterEric-Coroner\."
-set "file_list=%file_list% BepInEx\config\Translation\."
-set "file_list=%file_list% BepInEx\config\zh-CN\."
-
-set "file_list=%file_list% BepInEx\core\."
-
-set "file_list=%file_list% BepInEx\patchers\."
-set "file_list=%file_list% BepInEx\patchers\BepInEx.MonoMod.HookGenPatcher\."
-
-set "file_list=%file_list% BepInEx\plugins\."
-set "file_list=%file_list% BepInEx\plugins\HDLethalCompany\."
-set "file_list=%file_list% BepInEx\plugins\LethalConfig\."
-set "file_list=%file_list% BepInEx\plugins\MMHOOK\."
-set "file_list=%file_list% BepInEx\plugins\MoreEmotes\."
-set "file_list=%file_list% BepInEx\plugins\MoreShipUpgrades\."
-set "file_list=%file_list% BepInEx\plugins\ShipLoot\."
-set "file_list=%file_list% BepInEx\plugins\XUnity.AutoTranslator\."
 
 REM 循环遍历文件列表并删除文件
 for %%F in (%file_list%) do (
     if exist "%%F" (
-        del /f /q "%%F"
+        del /s /q "%%F"
         echo 删除文件 '%%F' 成功。
     ) else (
         echo 文件 '%%F' 不存在，无需删除。
@@ -42,10 +22,10 @@ for %%F in (%file_list%) do (
 REM 循环遍历文件夹列表并删除文件夹
 for %%D in (%folder_list%) do (
     if exist "%%D" (
-        rd /s /q "%%D"
-        echo 删除文件夹 '%%D' 成功。
+        rd /s /q "BepInEx\."
+        echo 删除文件夹 'BepInEx' 成功。
     ) else (
-        echo 文件夹 '%%D' 不存在，无需删除。
+        echo 文件夹 'BepInEx' 不存在，无需删除。
     )
 )
 
